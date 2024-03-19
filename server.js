609403aaa010d2c36702c121ad60c3e92a2b97b8
mmtrucefacts <93686164+mmtrucefacts@users.noreply.github.com>
2024-03-19 11:39:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Use the provided PORT environment variable, or default to 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
